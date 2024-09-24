d97c4f6e6f53fc2ef57fd7fa73aa7868f01556d2
Prince <mensahp5877@gmail.com>
2024-09-24 11:25:42 +0000

    Commit 1

---
+++ app/toggleDnN.js
@@ -0,0 +1,8 @@
+export default function toggleDnN(){
+    return (
+        <div className=" flex">
+            <button className=""><img src="./"/></button>
+            <button className=""></button>
+        </div>
+    );
+}
