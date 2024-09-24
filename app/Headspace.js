d24c188fa9bbe8bef883c3f87bb1df0bbad08a29
Prince <mensahp5877@gmail.com>
2024-09-24 12:46:37 +0000

    Deleted the svg's I added,

--- app/Headspace.js
+++ app/Headspace.js
@@ -1,9 +1,11 @@
 export default function Headspace(){
     return (
 
-        <div className=" flex">
-            <p className=" text-2xl">TO-DO.</p>
-
+        <div className=" flex justify-center">
+            <img className=" w-20 h-20 rounded-full" src="/logo1.jpg"/>
+            <p className=" self-center text-2xl ml-5">
+                TO-DO 
+            </p>
         </div>
     );
 }
--- app/page.js
+++ app/page.js
@@ -1,12 +1,13 @@
 import Image from "next/image";
 import Headspace
  from "./Headspace";
-import Title from "./Headspace";
+
+
 export default function Home(){
   return (
     <main className=" p-8">
         <nav>
-           <Title/>
+           <Headspace/>
         </nav>
     </main>
   )
