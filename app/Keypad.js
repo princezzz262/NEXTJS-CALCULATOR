7acb39aadb32f980f0a5c7478b26c60590a9a3d4
Prince <mensahp5877@gmail.com>
2024-09-24 13:31:49 +0000

    Contains keypad buttns code

--- app/Keypad.js
+++ app/Keypad.js
@@ -1,7 +1,7 @@
 export default function Keypad(){
     return(
         <section className="grid grid-template-rows-[repeat(5,1fr)] grid-template-cols-[repeat(4,1fr)]">
-            
+            <button className=" px-5 py-5 text-white"></button>
         </section>
     );
 }
